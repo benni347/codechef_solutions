9f9427e4d29f919c426f5a81fe0a329c15ef0a23
Cédric Skwar <cdrc@skwar.me>
2022-05-06 21:36:27 +0200

    Added league problem

--- farmers_league/league/__init__.py
+++ farmers_league/league/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This is my solution to the codechef problem farmers leaque.
+This is my solution to the codechef problem farmers league.
 URL:            "https://www.codechef.com/problems/LEAGUE"
 Problem Code:   LEAGUE
 Difficulty:     883
@@ -14,35 +14,40 @@ class Main:
     """
 
     def __init__(self) -> None:
+        self.second_place_points = None
         self.teams = 0
         self.win_point = 3
         self.lose_point = 2
-        self.first_place_points = 0
+        self.first_place_points = None
 
     def get_teams_amount(self):
         """
-        Get total teamand subtract two.
+        Get total teams and subtract two.
         """
         self.teams = int(input())
-        # Subtract two so that no one wins against team one and one so it doesn't loose to itself.
-        self.teams -= 2
+        # Subtract one so that no one wins against team one
+        self.teams -= 1
+        self.get_first_team_score()
 
     def get_first_team_score(self):
         """
         Get first team score.
         """
-        self.first_place_points = (self.teams + 1) * self.win_point
+        self.first_place_points = self.teams * self.win_point
+        self.get_min_to_get_second_place()
 
     def get_min_to_get_second_place(self):
         """
         Get the min points required for second place.
         """
+        self.second_place_points = 3 * ((self.teams) // 2)
+        self.get_result()
 
-        # first_place_points = (self.teams - 1) * self.win_point
+    def get_result(self):
+        result = self.first_place_points - self.second_place_points
+        print(result)
+        return result
         # second_place_points = (self.teams - 2) * self.win_point
-        # print("First place: " + str(first_place_points))
-        # print("Second place: " + str(second_place_points))
-        # print(first_place_points - second_place_points)
 
 
 if __name__ == '__main__':
