b769f23eca9e47d081a1b61f07a00a4cc81f3a90
Cédric Skwar <cdrc@skwar.me>
2022-05-06 16:57:22 +0200

    Added desc in league test

--- farmers_league/test/league_test.py
+++ farmers_league/test/league_test.py
@@ -6,11 +6,11 @@ import farmers_league.league
 class TestScore():
 
     def test_user_activation(self):
-        4
-        2
-        3
-        4
-        9
+        4  # Test cases
+        2  # Team amount
+        3  # Team amount
+        4  # Team amount
+        9  # Team amount
 
     # def test_user_points_update(self):
     #     pass
