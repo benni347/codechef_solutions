6991421e724579d5f84ae1cd07c44a800c38a082
Cédric Skwar <cdrc@skwar.me>
2022-12-27 08:50:39 +0100

    Added the "How many unattempted problems" problem

---
+++ python/how_many_unattempted/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This is my solution to the codechef problem squats.
+URL:            "https://www.codechef.com/problems/PRACLIST"
+Problem Code:   PRACLIST
+Difficulty:     264
+Author:         benni347@github.com
+"""
+
+if __name__ == "__main__":
+    input_ = input().split()
+    first = int(input_[0])
+    second = int(input_[1])
+    print(first - second)
